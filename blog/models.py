6a5bfe670ef894456092350fc11202b4b5be1acb
Akvilion <ianko.andrii@gmail.com>
2021-03-07 13:40:20 +0200

    implement SU

--- blog/models.py
+++ blog/models.py
@@ -3,22 +3,22 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
-class Port(models):
+class Post(models.Model):
     STATUS_CHOISE = (
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    slug = models.SlugField(max_length=250, unique_for_date='publish')  
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
-    published = models.DateTimeField(default=timezone.now)
+    publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOISE, default='draft')
 
     class Meta:
-        ordering = ('-publish')
+        ordering = ('-publish',)
 
     def __str__(self):
         return self.title

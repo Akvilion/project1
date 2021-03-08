dc22c3b6489978568708f25199657f549eba293d
Akvilion <ianko.andrii@gmail.com>
2021-03-08 20:35:51 +0200

    add forms.py

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
 
 
 def post_list(request):
@@ -32,3 +33,14 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 2
     template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.mothod == 'POST':
+        form = EmailPostForm(request.POST)  # інстранс від форми оголошеної в forms.py
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()  # якщо не POST, а GET по просто показати форму
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})

6b3b06a07b0ef1e09c876db3bc67522ee1d222b0
Akvilion <ianko.andrii@gmail.com>
2021-03-10 21:16:27 +0200

    implement form for Comment model

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,14 @@
 from django import forms
+from .models import Comment
 
 
-class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=25)
-    email = forms.EmailField()
-    to = forms.EmailField()
-    comments = forms.CharField(required=False, widget=forms.Textarea)
+# class EmailPostForm(forms.Form):
+#     name = forms.CharField(max_length=25)
+#     email = forms.EmailField()
+#     to = forms.EmailField()
+#     comments = forms.CharField(required=False, widget=forms.Textarea)
+
+class CommentForm(forms.Form):
+    class Meta:
+        model = Comment
+        fiels = ('name', 'email', 'body')
